48086bf60448a6a02031746bec6750a41cd2c678
saii15 <sayali.ingawale6@gmail.com>
2021-06-29 14:34:36 +0530

    first commit

---
+++ src/test/java/Demo/Maven/AppiumTest.java
@@ -0,0 +1,18 @@
+package Demo.Maven;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void Automation()
+	{
+		System.out.println("Appium");
+	}
+	
+	@Test
+	public void Manual()
+	{
+		System.out.println("Mobile");
+	}
+}
